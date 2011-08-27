13206667460c33b476fb6f3f6ddba06b369ff8dd
jthiele <jthiele@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-08-27 18:11:19 +0000

    new project homepage - first upload
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rnetlogo@2 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -1,51 +1,193 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <title>RNetLogo - ...and two worlds are yours</title>
 
-<!-- This is the project specific website template -->
-<!-- It can be changed as liked or replaced by other content -->
 
-<?php
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
-$domain=ereg_replace('[^\.]*\.(.*)$','\1',$_SERVER['HTTP_HOST']);
-$group_name=ereg_replace('([^\.]*)\..*$','\1',$_SERVER['HTTP_HOST']);
-$themeroot='http://r-forge.r-project.org/themes/rforge/';
+  <link href="style.css" rel="stylesheet" type="text/css" />
 
-echo '<?xml version="1.0" encoding="UTF-8"?>';
-?>
-<!DOCTYPE html
-	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en   ">
+  <script type="text/javascript" src="js/cufon-yui.js"></script>
+  <script type="text/javascript" src="js/arial.js"></script>
+  <script type="text/javascript" src="js/cuf_run.js"></script>
+</head>
 
-  <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<title><?php echo $group_name; ?></title>
-	<link href="<?php echo $themeroot; ?>styles/estilo1.css" rel="stylesheet" type="text/css" />
-  </head>
 
 <body>
 
-<!-- R-Forge Logo -->
-<table border="0" width="100%" cellspacing="0" cellpadding="0">
-<tr><td>
-<a href="http://r-forge.r-project.org/"><img src="http://<?php echo $themeroot; ?>/images/logo.png" border="0" alt="R-Forge Logo" /> </a> </td> </tr>
-</table>
+<div class="main">
+<div class="header">
+<div class="header_resize">
+<div class="clr"></div>
 
+<div class="logo">
+<h1><a href="http://rnetlogo.r-forge.r-project.org/">R<span>NetLogo</span></a><small>...and
+two worlds are yours</small></h1>
 
-<!-- get project title  -->
-<!-- own website starts here, the following may be changed as you like -->
+</div>
 
-<?php if ($handle=fopen('http://'.$domain.'/export/projtitl.php?group_name='.$group_name,'r')){
-$contents = '';
-while (!feof($handle)) {
-	$contents .= fread($handle, 8192);
-}
-fclose($handle);
-echo $contents; } ?>
+<img src="images/NLandR.png" alt="" height="241" width="298" />
+<div class="clr"></div>
 
-<!-- end of project description -->
+<div class="header_img"></div>
 
-<p> No content added. </p>
+</div>
 
-<p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
+</div>
+
+<div class="clr"></div>
+
+<div class="content">
+<div class="content_resize">
+<div class="mainbar">
+<div class="article">
+<h2><span>What is it?</span></h2>
+
+<div class="clr"></div>
+
+<p>The RNetLogo package delivers an interface to embed <a href="http://ccl.northwestern.edu/netlogo/" target="_blank">NetLogo</a>
+into the <a href="http://www.r-project.org/" target="_blank">GNU
+R</a> environment with headless (no Graphical User Interface) and
+interactive GUI mode. It provides functions to load models, execute
+commands, push values and to get values from
+NetLogo reporters. The interface is mostly equivalent to NetLogos'
+<a href="http://ccl.northwestern.edu/netlogo/docs/mathematica.html" target="_blank">Mathematica Link</a>.
+</p>
+
+</div>
+
+<div class="article">
+<h2><span>How to get it?</span></h2>
+
+<div class="clr"></div>
+
+<p>The easiest way is to type <i>install.packages("RNetLogo")</i>
+in
+your R shell - or for Windows users with the RGui to click on <i>Packages
+-&gt; Install
+package(s)...</i> and select RNetLogo. This will install the
+version hosted on <a href="http://cran.r-project.org/web/packages/RNetLogo/index.html" target="_bank">CRAN</a>. The latest package version
+will be available here on <a href="https://r-forge.r-project.org/R/?group_id=1202">R-Forge</a>.
+</p>
+
+</div>
+
+<div class="article">
+<h2><span>Where to find more information?</span></h2>
+
+<div class="clr"></div>
+
+<p>The first choice should always be the manual. All functions of
+the RNetLogo package come with a manual page. The complete manual as
+pdf is available on <a href="http://cran.r-project.org/web/packages/RNetLogo/index.html" target="_blank">CRAN</a>.
+Furthermore, there is a more than 30 pages tutorial, which comes with
+the package. Just browse to your installation path of the RNetLogo
+package and look into the folder <i>tutorial</i>.
+Moreover, there are code samples to all functions which can be found in
+the folder <i>examples/code_samples</i> in the
+installation path.
+A manuscript of a short overview article was submitted to <a href="http://www.methodsinecologyandevolution.org/view/0/index.html" target="_blank">Methods in Ecology and Evolution</a>.
+</p>
+
+</div>
+
+<div class="article">
+<h2><span>What else?</span></h2>
+
+<div class="clr"></div>
+
+<p>If you are looking for the other way around, i.e. embed R into
+NetLogo models, don't hestitate to try the <a href="http://netlogo-r-ext.berlios.de/" target="_blank">R-Extension
+for NetLogo</a>.
+There are two different version: R-Extension and RServe-Extension. The
+latter is designed to work if you're using RNetLogo. The first
+one won't work in conjunction with RNetLogo.
+</p>
+
+</div>
+
+</div>
+
+<div class="sidebar">
+<div class="gadget">
+<h2>Menu</h2>
+
+<div class="clr"></div>
+
+<ul class="sb_menu">
+
+  <li><a href="http://rnetlogo.r-forge.r-project.org/">Home</a></li>
+
+  <li><a href="https://r-forge.r-project.org/projects/rnetlogo/">Project
+summary</a></li>
+
+  <li><a href="https://r-forge.r-project.org/news/?group_id=1202">News</a></li>
+
+  <li><a href="https://r-forge.r-project.org/tracker/?group_id=1202">Tracker</a></li>
+
+  <li><a href="https://r-forge.r-project.org/mail/?group_id=1202">Mailing
+list</a></li>
+
+  <li><a href="https://r-forge.r-project.org/R/?group_id=1202">Packages</a></li>
+
+  <li><a href="https://r-forge.r-project.org/scm/?group_id=1202">Source
+Code</a></li>
+
+</ul>
+
+</div>
+
+</div>
+
+<div class="clr"></div>
+
+</div>
+
+</div>
+
+<div class="fbg">
+<div class="fbg_resize"><!--
+<div class="col c1">
+<h2><span></span></h2>
+</div>
+<div class="col c2">
+<h2><span></span></h2>
+<p></p>
+</div>
+--><!--
+<div class="col c3">
+-->
+<h2><span>Contact</span></h2>
+
+<p>The person behind the scene is <a href="http://www.uni-goettingen.de/en/72779.html" target="_blank">Jan C. Thiele</a>, member of the
+Department of Ecoinformatics, Biometrics and Forest Growth at the
+University of Goettingen, Germany.
+He works in close cooperation with <a href="http://www.ufz.de/index.php?en=3679" target="_blank">Volker
+Grimm</a> from the Helmholtz Center for Environmental Research -
+UFZ, Department of Ecological Modelling, Leipzig, Germany.
+</p>
+
+<!--
+</div>
+-->
+<div class="clr"></div>
+
+</div>
+
+<div class="footer">
+<p class="lf">&copy; Copyright <a href="http://www.uni-goettingen.de/en/72779.html" target="_blank">Jan
+C. Thiele</a>.</p>
+
+<p class="rf">Layout by Cool <a href="http://www.coolwebtemplates.net/">Website Templates</a></p>
+
+<div class="clr"></div>
+
+</div>
+
+</div>
+
+</div>
 
 </body>
 </html>
