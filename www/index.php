dfbf5eec2a229743d6ae544695a258ebbef148ae
jthiele <jthiele@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-06-11 08:24:13 +0000

    update of web page
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rnetlogo@50 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -81,9 +81,7 @@ will be available here on <a href="https://r-forge.r-project.org/R/?group_id=120
 <p>The first choice should always be the manual. All functions of
 the RNetLogo package come with a manual page. The complete manual as
 pdf is available on <a href="http://cran.r-project.org/web/packages/RNetLogo/index.html" target="_blank">CRAN</a>.
-Furthermore, a more than 30 pages tutorial has been submitted to <a href="http://www.jstatsoft.org" target="_blank">Journal of Statistical Software</a>. Currently, a pre-version comes with the RNetLogo package. 
-Just browse to your installation path of the RNetLogo
-package and look into the folder <i>tutorial</i>.
+Furthermore, a 30 pages tutorial has been accepted for publication by <a href="http://www.jstatsoft.org/v58/i02/" target="_blank">Journal of Statistical Software</a>.
 Moreover, there are code samples to all functions which can be found in
 the folder <i>examples/code_samples</i> in the
 installation path.
